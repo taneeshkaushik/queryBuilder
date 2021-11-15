861a7a0d9a3530100b9c754e3b7f17e0e0b9bb59
taneeshkaushik <42700358+taneeshkaushik@users.noreply.github.com>
2021-11-16 00:47:38 +0530

    file rename issue resolved

--- src/Theme/index.js
+++ src/Theme/index.js
@@ -1,18 +1,17 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
   palette: {
-    mode:'dark',
+    mode: "dark",
     primary: {
-      
-      main: '#3333ff',
-      contrastText: '#ffffff'
+      main: "#3333ff",
+      contrastText: "#ffffff",
     },
     secondary: {
-      main: '#002699',
-      
-      contrastText: '#ffffff',
-    }, 
+      main: "#002699",
+
+      contrastText: "#ffffff",
+    },
 
     contrastThreshold: 3,
 
